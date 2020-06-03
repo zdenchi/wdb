d4881480e150ce29d129d15898182aa9940e3cea
Denis Zadodorozhko <zdenis1990@gmial.com>
2020-06-03 17:49:35 +0300

    First commit. Project init.

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.send('Main page');
+});
+
+// 404 - For non-existing page
+app.get('*', (req, res) => res.send('404 Page Not Found'));
+
+app.listen(3000, () => console.log('App start http://127.0.0.1:3000'));
